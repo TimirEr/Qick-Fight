a8914933cf452dc6087bb0212a45c43e2f3c5d71
ernman <ernman@kth.se>
2023-12-06 01:33:44 +0100

    project outline

---
+++ src/resolvePromise.js
@@ -0,0 +1,26 @@
+export default function resolvePromise(prms, promiseState){
+
+    promiseState.promise = prms;
+    promiseState.data = null;
+    promiseState.error = null;
+
+
+    function sucessACB(result){
+        if(promiseState.promise === prms){
+            promiseState.data = result;
+        }
+    }
+
+    function failureACB(error){
+        if(promiseState.promise === prms){
+            promiseState.error = error;
+        }
+        
+    }
+
+    if(prms !== null){
+        prms.then(sucessACB).catch(failureACB)
+    }
+
+//vad är egentligen meningen med promise?
+}
