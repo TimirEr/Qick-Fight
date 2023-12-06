f44ac7e99234b467e4fa540f0b6e5e4616f7ec74
ernman <ernman@kth.se>
2023-12-06 01:09:42 +0100

    test

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@
 import {createRoot} from "react-dom/client";
 import ReactRoot from "./reactjs/ReactRoot.jsx";
 
-import { observable, configure, reaction } from "mobx";
+import { observable, configure } from "mobx";
 configure({ enforceActions: "never", });  // we don't use Mobx actions
 const reactiveModel= observable();
 
