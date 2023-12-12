d95ed6bfd4ed88c57ecdba710796fd0834c8c259
ziangw2001 <132618412+ziangw2001@users.noreply.github.com>
2023-12-12 16:16:56 +0100

    test

--- src/apiConfig.js
+++
@@ -1,6 +0,0 @@
-//LOGIN FÖR API (MÅSTE GÖRA SÅ ATT DEN ÄR GIT IGNORE)
-
-const BASE_URL="https://mmaapi.p.rapidapi.com/";
-const API_KEY="5fcae92888msh27b07a13601f6bfp19cec4jsn2dfbc7b5ab9d";
-
-export {BASE_URL,API_KEY};
