84a29ced3c99b9b846490d7e6b41a1fa1b0d2745
Ratimir Ernman <ernman@kth.se>
2023-12-18 14:59:51 +0100

    updated result look

--- src/views/showresultView.jsx
+++ src/views/showresultView.jsx
@@ -10,14 +10,13 @@ export default (props) => {
     }
 
     function addFighterToFavoriteACB(fighter){
-      console.log(Model);
       Model.setCurrentFavoriteFighter(fighter.entity.name);
+      console.log(Model);
     }
 
     function showFighter(fighter){
         return (
             <span key={fighter.entity.id}  className="fighterDetails">
-                <div>
               <p>
                 <strong>Name:</strong> {fighter.entity.name}
               </p>
@@ -30,48 +29,9 @@ export default (props) => {
               <p>
                 <strong>Record:</strong> Wins: {fighter.entity.wdlRecord.wins} - Losses: {fighter.entity.wdlRecord.losses} - Draws: {fighter.entity.wdlRecord.draws}
               </p>
-              </div><br />
-              
-              <p>
-                <strong> Career Statistics</strong> 
-              </p>
-
-              <p>
-              <strong> Striking</strong>
-              </p>
-
-            <p>
-              <>Strikes Landed/Round:</> {props.statsResults?.issued?.strikes?.total?.landed?.roundAvg}
-            </p>
-            <p>
-              <>Significant Strikes Avg/ Round:</> {props.statsResults?.issued?.significantStrikes?.total?.landed?.roundAvg} -  <>Head:</> {props.statsResults?.issued?.significantStrikes?.head?.landed?.roundAvg} - Body {props.statsResults?.issued?.significantStrikes?.body?.landed?.roundAvg} - Legs {props.statsResults?.issued?.significantStrikes?.legs?.landed?.roundAvg}
-            </p>
-
-            <p>
-              <strong> Grappling</strong>
-            </p>
-
-            <p>
-              <>Takedowns Attempted/15 min:</> {props.statsResults?.issued?.takedowns?.total?.attempted?.avg15min}
-            </p>
-            <p>
-            <>Takedowns Succeeed/15 min:</> {props.statsResults?.issued?.takedowns?.total?.succeeded?.avg15min}
-            </p>
-            
-            <p>
-            <>Takedowns Defended/15 min: </> {props.statsResults?.received?.takedowns?.total?.defended?.avg15min} out of {props.statsResults?.received?.takedowns?.total?.attempted?.avg15min}
-            </p>
-
-
-
-          <p>
-              <>Submission Average/15 min</> {props.statsResults?.issued?.submissions?.total?.attempted?.avg15min}
-            </p>
-
-             
             <div className="result_button">
               <button onClick={() => addFighterToFavoriteACB(fighter)} className="result_button1">Add to favorite</button>
-              <button onClick={handleHomeACB} className="result_button2">Back</button>
+              <button onClick={handleHomeACB} className="result_button2">compare</button>
             </div>
             </span>
             
@@ -89,3 +49,39 @@ return(<div>
 
 
 }
+
+
+/*
+
+
+
+ <p>
+                <strong> Career Statistics</strong> 
+              </p>
+             
+              <p>
+                <strong>Strikes Landed/Round:</strong> {props.statsResults?.issued?.strikes.total?.landed?.roundAvg}
+              </p>
+      
+              <p>
+                <strong>Significant Strikes Avg/ Round:</strong> {props.statsResults?.issued?.significantStrikes?.total?.landed?.roundAvg}
+              </p>
+               <p>
+                <> Head:</> {props.statsResults?.issued?.significantStrikes?.head?.landed?.roundAvg} - Body {props.statsResults?.issued?.significantStrikes?.body?.landed?.roundAvg} - Legs {props.statsResults?.issued?.significantStrikes?.legs?.landed?.roundAvg}
+              </p>
+      
+              <p>
+                <strong>Strikes Landed/Round:</strong> {props.statsResults?.issued.strikes?.total?.landed?.roundAvg}
+        </p>
+
+
+
+
+
+
+
+
+
+
+
+*/ 
