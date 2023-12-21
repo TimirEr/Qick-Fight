db8b46c84d0840790607457f9219fe18364bde9f
ziangw2001 <132618412+ziangw2001@users.noreply.github.com>
2023-12-21 01:52:00 +0100

    UPDATE about

--- src/views/aboutView.jsx
+++ src/views/aboutView.jsx
@@ -11,78 +11,52 @@ function AboutView(props){
  
     return(
 <div className="about">
-    <div className="aboutUs">
-        <h1>About us</h1>
-        <p>The Lion Does Not Turn Around When the Small Dog Barks</p>
-        <p>Interaction Programmering and the Dynamic Web DH2642 project</p>
-    </div>
+   
+<br />
+<h2> Group 36 </h2>
+<br />
+<div className="about_row">
 
-    <h2 className="team">Our Team</h2>
-    
-    <div className="row">
-        <div className="column">
-            <div className="card">
-                <img src="src/assets/salah.jpg" alt="alexP" className="image"/>
-                <div className="person">
-                    <h2>Salahudin Salah</h2>
-                    <p className="title">Developer</p>
-                    <p>Jesus eller Nunez? Ni skämtar!!!</p>
-                    <p>sasalah@kth.se</p>
-                    <p><button onClick={() => jumpToEmailACB("sasalah@kth.se")} className="button">Contact</button></p>
-                </div>
-            </div>
-        </div>
+<div className=" about_per1">
+    <h4>Salahudin Salah</h4><br />
+    <img src="src/assets/salah.jpg" alt="salah" className="about_image"/>
+    <p>Developer</p>
+    <p>sasalah@kth.se</p><br />
+    <p><button onClick={() => jumpToEmailACB("kazimov@kth.se")} >Contact</button></p>
+</div>
 
-        <div className="column">
-            <div className="card">
-                <img src="src/assets/aliK.jpg" alt="ali" className="image"/>
-                <div className="person">
-                    <h2>Ali Kazimov</h2>
-                    <p className="title">Developer</p>
-                    <p>Ni är inte redo för min comeback!</p>
-                    <p>kazimov@kth.se</p>
-                    <p><button onClick={() => jumpToEmailACB("kazimov@kth.se")} className="button">Contact</button></p>
-                </div>
-            </div>
-        </div>
+<div className=" about_per2">
+    <h4>Ali Kazimov</h4><br />
+    <img src="src/assets/aliK.jpg" alt="aliK" className="about_image"/>
+    <p>Developer</p>
+    <p>kazimov@kth.se</p><br />
+    <p><button onClick={() => jumpToEmailACB("kazimov@kth.se")} >Contact</button></p>
 
-        <div className="column">
-            <div className="card">
-                <img src="src/assets/ziangw.jpg" alt="ziang" className="image"/>
-                <div className="person">
-                    <h2>Ziang Wang</h2>
-                    <p className="title">Developer</p>
-                    <p>Onana DIN MAMMA!</p>
-                    <p>ziangw@kth.se</p>
-                    <p><button onClick={() => jumpToEmailACB("ziangw@kth.se")} className="button">Contact</button></p>
-                </div>
-            </div>
-        </div>
-
-        <div className="column">
-            <div className="card">
-                <img src="src/assets/ratimir.jpg" alt="ratimir" className="image"/>
-                <div className="person">
-                    <h2>Ratimir Ernman</h2>
-                    <p className="title">Developer</p>
-                    <p>It's Joever!</p>
-                    <p>ernman@kth.se</p>
-                    <p><button onClick={() => jumpToEmailACB("ernman@kth.se")} className="button">Contact</button></p>
-                </div>
-            </div>
-        </div>
-    </div>
 
 </div>
-    
-    
-    
-    
-    
-    
 
+<div className=" about_per3">
+    {/*<div className="about_per3_image"> akskaksakks</div>*/}
     
+    <h4>Ziang Wang</h4><br />
+    <img src="src/assets/ziangw.jpg" alt="ziangw" className="about_image"/>
+    <p>Group Leader & Developer</p>
+    <p>ziangw@kth.se</p><br />
+    <p><button onClick={() => jumpToEmailACB("kazimov@kth.se")} >Contact</button></p>
 
+</div>
+
+<div className=" about_per4">
+    <h4>Ratimir Ernman</h4><br />
+    <img src="src/assets/ratimir.jpg" alt="ratimir" className="about_image"/>
+    <p>Co Leader & Developer</p>
+    <p>ernman@kth.se</p><br />
+    <p><button onClick={() => jumpToEmailACB("kazimov@kth.se")} >Contact</button></p>
+    
+</div>
+</div>
+
+</div>
 
     );
 }
