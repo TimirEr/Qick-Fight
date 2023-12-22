39874076f76dd8014e1508871952dfa66796523b
Salahudin77 <75946473+Salahudin77@users.noreply.github.com>
2023-12-22 17:34:50 +0100

    Update navBarView.jsx

--- src/views/navBarView.jsx
+++ src/views/navBarView.jsx
@@ -1,1 +1,65 @@
+import "/src/style.css"
+import React from 'react'
+import Model from "../Model";
 
+
+function NavbarView(props){
+
+    function onClickHomeACB(){
+      window.location.hash = '#/';
+    }
+    function onClickSearchACB(){
+        window.location.hash = '#/homeSearch';
+    }
+    function onClickCompareACB(){
+        window.location.hash = '#/compare';
+    }
+    function onClickAbout() {
+        window.location.hash = "#/about";
+    }
+
+    function handleLogInACB(){
+      console.log(props);
+        props.setLoginStatus();
+    }
+
+    function handleLogOutACB(){
+        props.setLogoutStatus();
+    }
+
+    function testACB(){
+      console.log(props.user);
+      console.log(props.loginStatus);
+    }
+
+
+
+
+    return(<div className="navbar">
+      <ul>
+        <li onClick ={onClickHomeACB}>Home</li>
+        <li onClick ={onClickSearchACB}>Search</li>
+        <li onClick ={onClickCompareACB}>Compare</li>
+        <li onClick ={onClickAbout}>About</li>
+
+
+      {(props.user == null) ?  
+
+        (<li onClick={handleLogInACB}> Sign In </li> ) 
+          : 
+          (<> 
+          <li><img src={props.user.photoURL} alt="Profile" className="google_image"/></li>
+          <li> <h5> {props.user.displayName}, {props.user.email}</h5> </li>
+          <li onClick={handleLogOutACB}> Logga ut</li>
+            </>
+          )
+        }
+
+      </ul>
+    </div>
+    );
+
+}
+
+
+export default NavbarView;
